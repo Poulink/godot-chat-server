3270a2d2f003e650595096a72056b932bb66964c
Poulink <kolobochi123@gmail.com>
2026-02-12 18:29:48 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import asyncio
-import websockets
 import os
+from websockets.server import serve
+from websockets.exceptions import ConnectionClosedOK
 
 clients = set()
 
@@ -10,13 +11,32 @@ async def handler(websocket):
         async for message in websocket:
             for client in clients:
                 await client.send(message)
+    except ConnectionClosedOK:
+        pass
     finally:
         clients.remove(websocket)
 
+async def health_check(reader, writer):
+    request = await reader.read(1024)
+    if b"HEAD" in request or b"GET / " in request:
+        response = (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 2\r\n"
+            "\r\nOK"
+        )
+        writer.write(response.encode())
+        await writer.drain()
+    writer.close()
+
 async def main():
     port = int(os.environ.get("PORT", 10000))
-    async with websockets.serve(handler, "0.0.0.0", port):
-        print("Server started")
-        await asyncio.Future()
+
+    ws_server = await serve(handler, "0.0.0.0", port)
+    http_server = await asyncio.start_server(health_check, "0.0.0.0", port)
+
+    print("Server started")
+    await asyncio.gather(ws_server.wait_closed(), http_server.serve_forever())
 
 asyncio.run(main())
+
