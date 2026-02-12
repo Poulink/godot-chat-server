4e7ecbc0896c68b0e0dd6fdb18c21485beb3effd
Poulink <kolobochi123@gmail.com>
2026-02-12 18:25:21 +0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,22 @@
+import asyncio
+import websockets
+import os
+
+clients = set()
+
+async def handler(websocket):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in clients:
+                await client.send(message)
+    finally:
+        clients.remove(websocket)
+
+async def main():
+    port = int(os.environ.get("PORT", 10000))
+    async with websockets.serve(handler, "0.0.0.0", port):
+        print("Server started")
+        await asyncio.Future()
+
+asyncio.run(main())
